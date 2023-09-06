6e875847e2ea1c504b41c8772b5825cd94ab6273
devmianharoon <devmianharoon@gmail.com>
2023-09-06 11:55:45 +0500

    exercise 25

--- index.js
+++ index.js
@@ -300,3 +300,18 @@ console.log(color.includes("blue")); // true
 const fruits = ["banaba", "apple", "orange"];
 console.log(!fruits.includes("kiwi")); //true
 console.log(!fruits.includes("apple")); // false
+// exercise 25
+/*Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'. */
+var alien_color = ['green', 'yellow', 'red'];
+// • Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+if (alien_color.includes("green")) {
+    console.log("You Just Earn 5 Points");
+}
+// • Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+if (alien_color.includes("green")) {
+    console.log("You Just Earn 5 Points");
+}
+;
+if (!alien_color.includes("green")) {
+    console.log("You Just Earn 5 Points");
+}
