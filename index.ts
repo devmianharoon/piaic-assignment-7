91a74377526b69472d5ff29c9790e897a1743186
devmianharoon <devmianharoon@gmail.com>
2023-09-06 12:07:31 +0500

    exercise 26

--- index.ts
+++ index.ts
@@ -448,3 +448,27 @@ if (alien_color.includes("green")) {
 if (!alien_color.includes("green")) {
     console.log("You Just Earn 5 Points");
 }
+
+
+
+
+                                 //exercise 26
+// Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+// • If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+if (alien_color.includes("green")) {
+    console.log("You Just Earn 5 Points for shooting the alien.");
+}
+// • If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+else if (!alien_color.includes("green") ) {
+    console.log("You Just Earn 10 Points for shooting the alien.");
+  
+} ;
+
+// • Write one version of this program that runs the if block and another that runs the else block.
+
+if (alien_color.includes("green")) {
+    console.log("You Just Earn 5 Points for shooting the alien.");
+}else{
+    console.log("You Just Earn 10 Points for shooting the alien.");
+
+};
