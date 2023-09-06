51cf5b372d007c40cbf571687909e278b270e037
devmianharoon <devmianharoon@gmail.com>
2023-09-06 15:57:10 +0500

    exercise 34

--- index.ts
+++ index.ts
@@ -641,3 +641,27 @@ for (let index = 0; index <= numArray.length; index++) {
    }
     
 }
+                                        // exercise 34
+// Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a array, and then use a for loop to print the name of each pizza.
+let favPizza:string[]=["BBQ","bhari kabab","mali Boti","pepperoni"];
+for (let index = 0; index < favPizza.length; index++) {
+    console.log(favPizza[index]);
+    
+}
+
+// • Modify your for loop to print a sentence using the name of the pizza instead of printing just the name of the pizza. For each pizza you should have one line of output containing a simple statement like I like pepperoni pizza.
+
+for (let index = 0; index < favPizza.length; index++) {
+    console.log(`I Like ${favPizza[index]} pizza`);
+    
+}
+
+
+// • Add a line at the end of your program, outside the for loop, that states how much you like pizza. The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!
+
+console.log("I really love pizza!");
+
+                                               // exercise 35
+// Animals: Think of at least three different animals that have a common characteristic. Store the names of these animals in a list, and then use a for loop to print out the name of each animal. • Modify your program to print a statement about each animal, such as A dog would make a great pet. • Add a line at the end of your program stating what these animals have in common. You could print a sentence such as Any of these animals would make a great pet!
+
+let animals:string[]=["dog","cow","goat"];
