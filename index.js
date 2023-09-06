f76acef69683affeca601ec1f15176261684e519
devmianharoon <devmianharoon@gmail.com>
2023-09-06 16:08:54 +0500

    exercise 35

--- index.js
+++ index.js
@@ -489,3 +489,17 @@ console.log("I really love pizza!");
 // exercise 35
 // Animals: Think of at least three different animals that have a common characteristic. Store the names of these animals in a list, and then use a for loop to print out the name of each animal. • Modify your program to print a statement about each animal, such as A dog would make a great pet. • Add a line at the end of your program stating what these animals have in common. You could print a sentence such as Any of these animals would make a great pet!
 let animals = ["dog", "cow", "goat"];
+for (const animal of animals) {
+    if (animal === "dog") {
+        console.log(`A ${animal} would make a great pet.`);
+    }
+    else if (animal === "cow") {
+        console.log(`A ${animal} used to get Milk.`);
+    }
+    else if (animal === "goat") {
+        console.log(`A ${animal} is alos used to get Milk.`);
+    }
+    ;
+}
+console.log("what these animals have in common. ");
+console.log("these animal could ba a great pet!");
