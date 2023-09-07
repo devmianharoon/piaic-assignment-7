3b2297fb0962a9ef3b89b416ef8e5992c7e78682
devmianharoon <devmianharoon@gmail.com>
2023-09-07 12:33:51 +0500

    exercise 44

--- index.ts
+++ index.ts
@@ -822,4 +822,18 @@ const newArray:string[]=["wahab","butt","rana","tayyab"];
 show_magician(newArray);
 
 
-// 
+//                                           Exercise 44
+// Sandwiches: Write a function that accepts a array of items a person wants on a sandwich. The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that is being ordered. Call the function three times, using a different number of arguments each time.
+
+
+function sandwich(items:string[]) {
+    console.log("You ordered a sandwich with the following ingredients:");
+    for (const item of items) {
+    console.log(`- ${item}`);
+} 
+}
+
+const orderSandwich=["Ham", "Cheese", "Lettuce", "Tomato"];
+sandwich(orderSandwich);
+
+
